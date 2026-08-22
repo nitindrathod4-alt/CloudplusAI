6c5fdeaafdb2148a7e3521c8fa824c960dfacfe1
Nitin Rathod <nitindrathod4@gmail.com>
2026-08-23 01:22:00 +0530

    infra: add Terraform outputs

---
+++ terraform/outputs.tf
@@ -0,0 +1,14 @@
+output "project_name" {
+  description = "CloudplusAI project name"
+  value       = var.project_name
+}
+
+output "environment" {
+  description = "Deployment environment"
+  value       = var.environment
+}
+
+output "aws_region" {
+  description = "Configured AWS region"
+  value       = var.aws_region
+}
